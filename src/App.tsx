ebcec4b2a3f986f71228ff771b952b541d6a6750
Starikova Maria <maria050200@gmail.com>
2025-11-18 20:15:23 +0500

    feat: add header component with navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Layout } from 'antd';
 import { AppRouter } from './router/AppRouter';
 import { Header } from './components/Header';
+import type { UserRole } from './components/index.ts';
 
+const mockData = {
+  role: 'client' as UserRole,
+  hasUnreadMessages: true
+};
 
 function App() {
   return (
     <Layout className="min-h-screen">
-      <Header />
-        <AppRouter />
+      <Header role={mockData.role} hasUnreadMessages={mockData.hasUnreadMessages} />
+      <AppRouter />
     </Layout>
   );
 }
