6a6bba5be2da7d3ed52356f869583166c9098eab
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-11-21 12:42:17 +0300

    feat: Добавить cookie.

--- backend/routes/auth.routes.ts
+++ backend/routes/auth.routes.ts
@@ -6,6 +6,9 @@ import loginSchema from '../schemas/auth/login.schema.js'
 import { RegisterDTO, UserWithToken, LoginDTO, RefreshBody } from '../types/auth.js'
 import { loginUser, registerUser } from 'controllers/user.js'
 import { refreshTokenService } from 'services/refreshToken.service.js'
+import { MAX_AGE_30_DAYS } from 'consts/cookie.js'
+import { ApiError } from 'utils/ApiError.js'
+import { setRefreshCookie } from 'utils/setRefreshCookie.js'
 
 export default async function authRoutes(fastify: FastifyInstance) {
 	fastify.post<{ Body: RegisterDTO; Reply: UserWithToken }>(
@@ -14,7 +17,14 @@ export default async function authRoutes(fastify: FastifyInstance) {
 		async (req, reply) => {
 			const user = await registerUser(req.body)
 
-			return reply.status(201).send(user)
+			setRefreshCookie(reply, user.token.refreshToken, MAX_AGE_30_DAYS)
+
+			return reply.status(201).send({
+				user: user.user,
+				token: {
+					accessToken: user.token.accessToken,
+				},
+			})
 		},
 	)
 
@@ -24,18 +34,36 @@ export default async function authRoutes(fastify: FastifyInstance) {
 		async (req, reply) => {
 			const user = await loginUser(req.body)
 
-			return reply.status(200).send(user)
+			setRefreshCookie(reply, user.token.refreshToken, MAX_AGE_30_DAYS)
+
+			return reply.status(200).send({
+				user: user.user,
+				token: {
+					accessToken: user.token.accessToken,
+				},
+			})
 		},
 	)
 
 	fastify.post<{ Body: RefreshBody; Reply: UserWithToken }>(
 		'/refresh-token',
 		async (req, reply) => {
-			const { refreshToken } = req.body
+			const refreshToken = req.cookies.refreshToken
+
+			if (!refreshToken) {
+				throw ApiError.unauthorized('Refresh token отсутствует')
+			}
 
 			const result = await refreshTokenService({ refreshToken })
 
-			return reply.status(200).send(result)
+			setRefreshCookie(reply, result.token.refreshToken, MAX_AGE_30_DAYS)
+
+			return reply.status(200).send({
+				user: result.user,
+				token: {
+					accessToken: result.token.accessToken,
+				},
+			})
 		},
 	)
 }
