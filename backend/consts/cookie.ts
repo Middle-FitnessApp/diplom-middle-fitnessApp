59a60204635764986745cb572adc8fb2cb82138a
Дмитрий <suvstreet@gmail.com>
2025-11-22 11:11:11 +0300

    feat: #86 Добавить cookie и jwt токены
    * fix: Добавить типы для jsonwebtoken.
    
    * feat: Добавить ручку с refresh токеном.
    
    * feat: #71(частично) Добавить middleware для проверки авторизации.
    
    * feat: Добавить cookie.

---
+++ backend/consts/cookie.ts
@@ -0,0 +1,3 @@
+const MAX_AGE_30_DAYS = 60 * 60 * 24 * 30
+
+export { MAX_AGE_30_DAYS }
---
+++ backend/services/refreshToken.service.ts
@@ -0,0 +1,40 @@
+import { prisma } from '../prisma.js'
+import { ApiError } from '../utils/ApiError.js'
+import { RefreshBody } from 'types/auth.js'
+import { generateAccessToken, generateRefreshToken } from './token.service.js'
+
+export async function refreshTokenService({ refreshToken }: RefreshBody) {
+	const tokenRecord = await prisma.refreshToken.findUnique({
+		where: { token: refreshToken },
+		include: { user: true },
+	})
+
+	if (!tokenRecord) {
+		throw ApiError.unauthorized('Неверный или устаревший refresh token')
+	}
+
+	const user = tokenRecord.user
+
+	// генерируем новые токены
+	const accessToken = generateAccessToken(user.id)
+	const newRefreshToken = await generateRefreshToken(user.id)
+
+	// обновляем токен в базе
+	await prisma.refreshToken.update({
+		where: { token: refreshToken },
+		data: { token: newRefreshToken },
+	})
+
+	return {
+		token: {
+			accessToken,
+			refreshToken: newRefreshToken,
+		},
+		user: {
+			id: user.id,
+			name: user.name,
+			email: user.email,
+			phone: user.phone,
+		},
+	}
+}
